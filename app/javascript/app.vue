8abda6c66a6a3e89a473d6404bec486890332a66
Yasunari_Kainuna <yasu6491@icloud.com>
2021-03-04 19:51:41 +0900

    [add]vue.jsでace組み込み

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,90 @@
+<!--<template>-->
+<!--  <div id="app">-->
+<!--    <div style="width: 20rem; height: 10rem">-->
+<!--      <Ace />-->
+<!--    </div>-->
+<!--  </div>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--import Ace from './ace'-->
+
+<!--export default {-->
+<!--  name: 'App',-->
+<!--  components: {-->
+<!--    Ace-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
+
+<!--<style>-->
+<!--#app {-->
+<!--  font-family: Avenir, Helvetica, Arial, sans-serif;-->
+<!--  -webkit-font-smoothing: antialiased;-->
+<!--  -moz-osx-font-smoothing: grayscale;-->
+<!--  color: #2c3e50;-->
+<!--}-->
+<!--</style>-->
+
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    Answer editor
+    <Ace
+        mode="html"
+        theme="github"
+        :content="contentJs"
+        @content-changed="onChangeJs"
+        style="height: 10rem"
+    />
+
+    <div class="box">
+      <p>JavaScript content</p>
+      <textarea
+          v-model="contentJs"
+          disabled
+          class="preview"
+      />
+      <p>{{ question }}</p>
+      <p>{{ answer }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import Ace from './ace'
 export default {
-  data: function () {
+  name: 'App',
+  components: {
+    Ace
+  },
+  data() {
     return {
-      message: "app.vue"
+      contentJs: `初期値`,
+      question: `question`,
+      answer: 'answer',
+    }
+  },
+  methods: {
+    onChangeJs(data) {
+      this.contentJs = data;
     }
   }
 }
 </script>
 
-<style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+.box {
+  display: inline-block;
+  width: 20rem;
+}
+.preview {
+  width: 100%;
+  height: 10rem;
 }
 </style>
